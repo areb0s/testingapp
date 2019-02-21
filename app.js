20a16c8f1d066445976bfd3f223e9662389fdbe1
Arebos <lent1510@gmail.com>
2019-02-21 15:15:18 +0900

    test

--- app.js
+++ app.js
@@ -9,23 +9,30 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-	next(createError(404));
+  next(createError(404));
 });
 
-app.use(function (req, res, next) {
-	console.log(req.user);
-	if (!req.user) return next(createError(401, 'Please login to view this page.'))
-	next()
-})
+// PORT
+const port = process.env.PORT || 3001;
+
+app.use(function(req, res, next) {
+  console.log(req.user);
+  if (!req.user)
+    return next(createError(401, 'Please login to view this page.'));
+  next();
+});
 
 // error handler
 app.use((err, req, res) => {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-module.exports = app;
+// SERVER LISTEN
+app.listen(port, () => {
+  console.log('SERVER ON PORT', port);
+});
